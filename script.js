508861b2179aa19c903e9b6173457b1b4639c873
higuchi <higuchi381130@gmail.com>
2024-09-29 17:24:02 +0900

    js使ってnewtの内容表示させたい！

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", async () => {
-    const API_ENDPOINT = "https://otenkiame.api.newt.so/v1/blog/article";
+    const API_ENDPOINT = "https://otenkiame.cdn.newt.so/v1/blog/article"; 
     // https://<スペースのUID>.cdn.newt.so/v1/<AppのUID>/<モデルのUID>
     const API_TOKEN = "K4euByk5DGqL5D7WfsaaL9HLN5bY6Kq8oev0OTnNWVk";
   
